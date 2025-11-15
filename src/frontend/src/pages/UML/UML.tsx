f2fc735a828b190598dc75269a6f5ca5380ee827
James_AF_Fitzsimons <u21516741@tuks.co.za>
2025-11-15 15:42:52 +0200

    UML: editor

--- src/frontend/src/pages/UML/UML.tsx
+++ src/frontend/src/pages/UML/UML.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import ApollonEditor from "@ls1intum/apollon";
 
 const UML: React.FC = () => {
   return (
---
+++ src/frontend/src/types/apollon.d.ts
@@ -0,0 +1,3 @@
+declare module "@ls1intum/apollon";
+declare module "@ls1intum/apollon/lib/es6";
+declare module "@ls1intum/apollon/lib/es5";
